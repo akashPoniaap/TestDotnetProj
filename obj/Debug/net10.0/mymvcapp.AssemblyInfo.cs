99b5dd0f6be0462c4f3e9478928f3de8ee3e1d5b
Akash Ponia <akashp@unifycloud.com>
2026-04-15 13:15:49 +0530

    new update

--- obj/Debug/net10.0/mymvcapp.AssemblyInfo.cs
+++ obj/Debug/net10.0/mymvcapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mymvcapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a976397cdfe09ed5556506a3e486eb0365476f")]
 [assembly: System.Reflection.AssemblyProductAttribute("mymvcapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mymvcapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
